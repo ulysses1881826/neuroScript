78e3e97e31a3dce6c9b0b8310e35e9fa1e3010b3
Ubuntu <ubuntu@ulyhash-spm.us-west1-b.c.gifted-airport-269213.internal>
2020-03-06 13:56:32 +0000

    freesurfer out to csv

--- downloadFreesurferOutput.sh
+++ downloadFreesurferOutput.sh
@@ -14,7 +14,7 @@ fi
 for subject in $(jq -r '.[].meta.subject' all.json | sort -u)
 do
     echo "downloading subject:$subject ---------------"
-    mkdir -p MCI_freesurfer_output/$subject/
+#    mkdir -p MCI_freesurfer_output/$subject/
     ids=$(jq -r '.[] | select(.meta.subject == '\"$subject\"') | ._id' all.json)
     for id in $ids
     do
@@ -22,6 +22,7 @@ do
 #        tags=$(jq -r '.[] | select(._id=='\"$id\"') | .tags | join(".")' all.json)
         outdir=MCI_freesurfer_output/$subject
         if [ ! -d $outdir ]; then
+	    mkdir -p MCI_freesurfer_output/$subject 
             bl dataset download $id --directory $outdir
         fi
     done
