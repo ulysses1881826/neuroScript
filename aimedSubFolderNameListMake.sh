bb9607e302120b0db792a97d7a529f8877c10d8c
Ubuntu <ubuntu@instance-1.us-west1-b.c.golden-pillar-264109.internal>
2020-03-12 07:14:33 +0000

    update

--- aimedSubFolderNameListMake.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat MCI_amyloid_pos.csv | grep _S_ | awk -F "," '{print $1}' > folderName.txt
-file=folderName.txt
-for i in $(cat $file); do
-    tree -d ./MM1/$i >> aimedSubFolderNameList.txt
-done
--- updateTags.sh
+++ updateTags.sh
@@ -2,18 +2,61 @@
 
 set -e
 
-project=5e563d200f7fa60c683c8fe2
-datatype="neuro/anat/t1w"
 
+MCI_project=5e44ab0a1eafff87c0f93fe3
+CN_project=5e563d200f7fa60c683c8fe2
+AD_project=5e5f781b82b37fb68b8fa0c9
+anat_datatype="neuro/anat/t1w"
+freesurfer_datatype="neuro/freesurfer"
+par_datatype="neuro/parcellation/volume"
+
+##############################################Update CN tags##############################################
 #cache the list of datasets that we could download
-if [ ! -f all.json ]; then
-    bl dataset query --limit 10000 --project $project --datatype $datatype --json > all.json
+if [ ! -f CN_freesufer_all.json ]; then
+    bl dataset query --limit 10000 --project $CN_project --datatype $freesurfer_datatype --json > CN_freesufer_all.json
 fi
 
 #enumerate subjects
-for subject in $(jq -r '.[].meta.subject' all.json | sort -u)
+for CN_subject in $(jq -r '.[].meta.subject' CN_freesufer_all.json | sort -u)
 do
-    echo $subject
-    id=$(jq -r '.[] | select(.meta.subject == '\"$subject\"') | select(.prov.task.config._outputs[].tags == '[]') | ._id' all.json)
-    bl dataset update --id $id --add_tag "CN_amyloid_pos"
+    echo $CN_subject
+#     id=$(jq -r '.[] | select(.meta.subject == '\"$subject\"') | select(.prov.task.config._outputs[].tags == '[]') | ._id' CN_freesufer_all.json)
+    CN_id=$(jq -r '.[] | select(.meta.subject == '\"$CN_subject\"') | ._id' CN_freesufer_all.json)
+    bl dataset update --id $CN_id --add_tag "CN_freesurfer"
 done
+
+
+##############################################Update MCI tags##############################################
+#cache the list of datasets that we could download
+if [ ! -f MCI_freesufer_all.json ]; then
+    bl dataset query --limit 10000 --project $MCI_project --datatype $freesurfer_datatype --json > MCI_freesufer_all.json
+fi
+
+#enumerate subjects
+for MCI_subject in $(jq -r '.[].meta.subject' MCI_freesufer_all.json | sort -u)
+do
+    echo $MCI_subject
+#     id=$(jq -r '.[] | select(.meta.subject == '\"$subject\"') | select(.prov.task.config._outputs[].tags == '[]') | ._id' MCI_freesufer_all.json)
+    MCI_id=$(jq -r '.[] | select(.meta.subject == '\"$MCI_subject\"') | ._id' MCI_freesufer_all.json)
+    bl dataset update --id $MCI_id --add_tag "MCI_freesurfer"
+done
+
+
+##############################################Update AD tags##############################################
+#cache the list of datasets that we could download
+if [ ! -f AD_freesufer_all.json ]; then
+    bl dataset query --limit 10000 --project $AD_project --datatype $freesurfer_datatype --json > AD_freesufer_all.json
+fi
+
+#enumerate subjects
+for AD_subject in $(jq -r '.[].meta.subject' AD_freesufer_all.json | sort -u)
+do
+    echo $AD_subject
+#     id=$(jq -r '.[] | select(.meta.subject == '\"$subject\"') | select(.prov.task.config._outputs[].tags == '[]') | ._id' AD_freesufer_all.json)
+    AD_id=$(jq -r '.[] | select(.meta.subject == '\"$AD_subject\"') | ._id' AD_freesufer_all.json)
+    bl dataset update --id $AD_id --add_tag "AD_freesurfer"
+done
+
+
+
+
